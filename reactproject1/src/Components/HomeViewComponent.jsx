7844fac8129b188eabe7ce5089dc95e170fa14f1
thoma200 <thoma200@csusm.edu>
2025-03-19 00:07:30 -0700

    Homeview cook in progress

--- reactproject1/src/Components/HomeViewComponent.jsx
+++ reactproject1/src/Components/HomeViewComponent.jsx
@@ -23,21 +23,32 @@ const HomeViewComponent = () => {
                         className="flex flex-col h-100/100 w-30/100">
                         <div 
                             id="profile_pic_container"
-                            className="flex justify-center items-center w-[128px] h-[128px] rounded-[5px] bg-[white] mt-[7vh] ml-[5vw]">
+                            className="flex justify-center items-center w-[128px] h-[128px] rounded-[5px] bg-[#bbb] mt-[7vh] ml-[5vw]">
                             <div
                                 id="profilepic"
                                 className="w-[100px] h-[100px] rounded-[50%] bg-[green]"></div>
                         </div>
                         <div
                             id="profile_blurb_container"
-                            className="flex flex-col flex-grow w-100/100 justify-start pl-14 pt-4">
-                            <div id="profile_blurb">Jacob Thomas</div>
-                            <div id="profile_blurb">Level 25</div>
-                            <div id="profile_blurb">Programmer</div>
+                            className="flex flex-col flex-grow w-100/100 justify-start pl-[4vw] pt-[2vh]">
+
+                            <div className="profile_blurb">Jacob Thomas</div>
+                            <div className="profile_blurb">Level 25</div>
+                            <div className="profile_blurb">Programmer</div>
                         </div>
                     </div>
                     <div id="skills_container"
-                        className="w-70/100 h-100/100 ml-[3vw]" ></div>
+                        className="relative flex flex-col w-70/100 h-100/100 ml-[3vw]" >
+
+                        <div className="skill_display w-fit flex">
+                            <img className="w-[64px] h-[64px]" src="/src/assets/icons/c-.png"></img>
+                            <div class="absolute inset-x-0 translate-y-[32px] translate-x-[64px] border-t-[2px] border-[#000]"></div>
+                        </div>
+                        <div className="skill_display"></div>
+                        <div className="skill_display"></div>
+                        <div className="skill_display"></div>
+                    </div>
+                    
                 </div>
             </div>
         </>
