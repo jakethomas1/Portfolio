68daf16b4251a021ce729366ec8d314113017222
jakethomas1 <jacobmthomasb@gmail.com>
2025-04-03 23:11:24 -0700

    Disable creating certificate

--- reactproject1/vite.config.js
+++ reactproject1/vite.config.js
@@ -9,35 +9,35 @@ import path from 'path';
 import child_process from 'child_process';
 import { env } from 'process';
 
-const baseFolder =
-    env.APPDATA !== undefined && env.APPDATA !== ''
-        ? `${env.APPDATA}/ASP.NET/https`
-        : `${env.HOME}/.aspnet/https`;
+//const baseFolder =
+//    env.APPDATA !== undefined && env.APPDATA !== ''
+//        ? `${env.APPDATA}/ASP.NET/https`
+//        : `${env.HOME}/.aspnet/https`;
 
-const certificateName = "reactproject1.client";
-const certFilePath = path.join(baseFolder, `${certificateName}.pem`);
-const keyFilePath = path.join(baseFolder, `${certificateName}.key`);
+//const certificateName = "reactproject1.client";
+//const certFilePath = path.join(baseFolder, `${certificateName}.pem`);
+//const keyFilePath = path.join(baseFolder, `${certificateName}.key`);
 
-if (!fs.existsSync(baseFolder)) {
-    fs.mkdirSync(baseFolder, { recursive: true });
-}
+//if (!fs.existsSync(baseFolder)) {
+//    fs.mkdirSync(baseFolder, { recursive: true });
+//}
 
-if (!fs.existsSync(certFilePath) || !fs.existsSync(keyFilePath)) {
-    if (0 !== child_process.spawnSync('dotnet', [
-        'dev-certs',
-        'https',
-        '--export-path',
-        certFilePath,
-        '--format',
-        'Pem',
-        '--no-password',
-    ], { stdio: 'inherit', }).status) {
-        throw new Error("Could not create certificate.");
-    }
-}
+//if (!fs.existsSync(certFilePath) || !fs.existsSync(keyFilePath)) {
+//    if (0 !== child_process.spawnSync('dotnet', [
+//        'dev-certs',
+//        'https',
+//        '--export-path',
+//        certFilePath,
+//        '--format',
+//        'Pem',
+//        '--no-password',
+//    ], { stdio: 'inherit', }).status) {
+//        throw new Error("Could not create certificate.");
+//    }
+//}
 
-const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}` :
-    env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'https://localhost:7090';
+//const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}` :
+//    env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'https://localhost:7090';
 
 // https://vitejs.dev/config/
 export default defineConfig({
