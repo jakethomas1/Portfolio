4940f4ebc8075279d5a101d73629d0e744328763
jakethomas1 <jacobmthomasb@gmail.com>
2025-04-20 16:50:16 -0700

    menu button fully functional on mobile now.

--- reactproject1/src/Components/NavBarComponent.jsx
+++ reactproject1/src/Components/NavBarComponent.jsx
@@ -4,14 +4,19 @@ import ContactComponent from "./ContactComponent";
 import { BrowserRouter as Router, Link, Routes, Route } from 'react-router-dom';
 import ProjectComponent from './ProjectComponent';
 import { useState } from "react";
+import { useClickAway } from '../utils/useClickAway';
 
 const NavBarComponent = () => {
     const [vis, setVis] = useState(false);
-    
+
     const jumpTo = (element_id) => {
         const projectsview = document.getElementById(element_id);
         projectsview.scrollIntoView({ behavior: 'auto', block: 'start' });
     };
+
+    const navref = useClickAway(() => {
+        setVis(false)
+    });
 
     return (
         <>
@@ -25,16 +30,27 @@ const NavBarComponent = () => {
                 <div className="w-full h-[4px] bg-white rounded-full"></div>
                 <div className="w-full h-[4px] bg-white rounded-full"></div>
             </div>
-            <div id="navbar" className={`md:border-[rgba(0,0,0,.25)] md:border-solid md:border-[1px] md:bg-[#fcfae6] md:min-w-[480px] 
+            <div
+                ref={navref}
+                id="navbar"
+                className={`md:border-[rgba(0,0,0,.25)] md:border-solid md:border-[1px] md:bg-[#fcfae6] md:min-w-[480px] 
                                         md:w-40/100 md:h-6/100 md:top-1/100 md:fixed md:rounded-[6px] md:z-20 md:left-1/2 md:-translate-x-1/2
                                         fixed w-auto max-w-7/10 h-full bg-[rgba(200,200,200,1)] z-20 left-0 md:block ${vis ? 'block' : 'hidden'}`} >
                 <Router>
                     <nav className="w-full h-full">
                         <div className="nav_link_container flex flex-col md:flex-row h-full w-full md:justify-center md:items-center text-left md:text-middle">
-                            <Link to="/home" onClick={() => jumpTo('homeview')} className={`nav_link`}>Home</Link>
-                            <Link to="/projects" onClick={() => jumpTo('projectsview')} className="nav_link">Projects</Link>
-                            <Link to="/about" onClick={() => jumpTo('timelineview_container')} className="nav_link">About</Link>
-                            <Link to="/contact" onClick={() => jumpTo('footer')} className="nav_link">Contact</Link>
+                            <Link to="/home" onClick={() => { jumpTo('homeview'); setVis(false); }} className={`nav_link`}>Home</Link>
+                            <Link to="/projects" onClick={() => { jumpTo('projectsview'); setVis(false); }} className="nav_link">Projects</Link>
+                            <Link to="/about" 
+                                onClick={() => { jumpTo('timelineview_container'); setVis(false); }}
+                                className="nav_link">About</Link>
+                            <Link
+                                to="/contact"
+                                onClick={() => {
+                                jumpTo('footer')
+                                setVis(false)
+                            }}
+                                className="nav_link">Contact</Link>
                         </div>
                     </nav>
                     <Routes>
